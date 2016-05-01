bb21ae92870948cbfb0639c30e0cb216b5ff5784
HSuzu <henry.shinji@gmail.com>
2016-05-01 14:39:32 -0300

    Update getchar.py

--- Trabalhos/Trabalho-01/getchar.py
+++ Trabalhos/Trabalho-01/getchar.py
@@ -1,4 +1,5 @@
-# Ambas formas funcionam, segundo os autores, para Win, Linux e OSX
+# http://stackoverflow.com/questions/510357/python-read-a-single-character-from-the-user
+# http://code.activestate.com/recipes/134892/
 class _Getch:
     """Gets a single character from standard input.  Does not echo to the
 screen."""
@@ -37,29 +38,3 @@ class _GetchWindows:
 
 
 getch = _Getch()
-
-'''
-def _find_getch():
-    try:
-        import termios
-    except ImportError:
-        # Non-POSIX. Return msvcrt's (Windows') getch.
-        import msvcrt
-        return msvcrt.getch
-
-    # POSIX system. Create and return a getch that manipulates the tty.
-    import sys, tty
-    def _getch():
-        fd = sys.stdin.fileno()
-        old_settings = termios.tcgetattr(fd)
-        try:
-            tty.setraw(fd)
-            ch = sys.stdin.read(1)
-        finally:
-            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
-        return ch
-
-    return _getch
-
-getch = _find_getch()
-'''
